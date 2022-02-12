d679e6d791c3d225f2d1f10bb92316422b1fd773
Audarilanka <dissanayakelanka96@gmail.com>
2022-02-12 12:32:06 +0530

    feat(challenge 6):PlusMinus

---
+++ Hackerrank/PlusMinus.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class PlusMinus {
+
+    public static void main(String[] args) {
+        Scanner array = new Scanner(System.in);
+        int n = array.nextInt();
+        int positive = 0;
+        int zero = 0;
+        int negative = 0;
+        for (int i = 0; i < n; i++) {
+            int a = array.nextInt();
+            if (a > 0) {
+                positive++;
+            } else if (a == 0) {
+                zero++;
+            } else {
+                negative++;
+            }
+        }
+        System.out.println(positive / (double) n);
+        System.out.println(negative / (double) n);
+        System.out.println(zero / (double) n);
+        array.close();
+    }
+
+}
+
+
