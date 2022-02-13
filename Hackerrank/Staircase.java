9d9a15b666af25aedac4b8af0f29aabc9b8ff0a6
Audarilanka <dissanayakelanka96@gmail.com>
2022-02-13 20:22:47 +0530

    feat(challenge 7); Staircase

---
+++ Hackerrank/Staircase.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Staircase {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i; k++)
+                System.out.print("#");
+
+            System.out.println();
+            in.close();
+
+        }
+
+    }
+
+}
