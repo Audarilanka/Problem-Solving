1a14994013d9de5750491b51564afa2167935753
Audarilanka <dissanayakelanka96@gmail.com>
2022-02-15 14:22:25 +0530

    feat(challenge 9): Birthday Cake

--- Hackerrank/BirthdayCakeCandles.java
+++ Hackerrank/BirthdayCakeCandles.java
@@ -1,28 +1,25 @@
 
 import java.io.*;
 import java.util.*;
-
 public class BirthdayCakeCandles {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
+
         int n = in.nextInt();
         int max = 0;
         int sum = 0;
         int num;
-        for(int i =0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             num = in.nextInt();
-            if(num > max){
+            if (num > max) {
                 sum = 1;
                 max = num;
-            }else if(num == max){
+            } else if (num == max) {
                 sum++;
             }
         }
         System.out.println(sum);
-        
+
     }
 }
-                 
-                 
