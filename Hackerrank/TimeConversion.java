1944696c5eac2069c09cb4c4bda36ee4c3e4048b
Audarilanka <dissanayakelanka96@gmail.com>
2022-02-16 10:34:37 +0530

    feat(Challenge 10): Time Conversion

---
+++ Hackerrank/TimeConversion.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+import java.text.*;
+
+
+public class TimeConversion {
+
+       public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String a = in.nextLine(); 
+        DateFormat inFormat = new SimpleDateFormat( "hh:mm:ssaa");
+        DateFormat outFormat = new SimpleDateFormat( "HH:mm:ss");
+
+        Date date = null;
+        try {
+            date = inFormat.parse(a); 
+        }catch (ParseException b ){
+            b.printStackTrace();
+        }
+        if( date != null ){
+            String myDate = outFormat.format(date);
+            System.out.println(myDate);
+        }
+    }
+
+}
+
+
