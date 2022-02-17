3add0ac63e0ef3ba3ceca3c1c1c19a0ea4d53bc9
Audarilanka <dissanayakelanka96@gmail.com>
2022-02-17 10:13:03 +0530

    feat(challeng 11):GradingStudents

---
+++ Hackerrank/GradingStudens.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+
+public class GradingStudents {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int a0 = 0; a0 < n; a0++){
+            int grade = in.nextInt();
+            if (grade >= 38) {
+                if (grade % 5 >= 3) {
+                    grade += 5 - (grade % 5);
+                }
+            }
+            System.out.println(grade);
+        }
+    }
+}
+
