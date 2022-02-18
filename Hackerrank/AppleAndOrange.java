808d76612feb8a9ce0ca379d9262a5d5c337c586
Audarilanka <dissanayakelanka96@gmail.com>
2022-02-18 10:23:46 +0530

    feat(challenges 12):AppleAndOrange

--- Hackerrank/AppleAndOrange.java
+++ Hackerrank/AppleAndOrange.java
@@ -1,7 +1,5 @@
 import java.util.*;
 
-
-
 public class AppleAndOrange {
 
     public static void main(String[] args) {
@@ -14,7 +12,7 @@ public class AppleAndOrange {
         int n = in.nextInt();
         int[] apple = new int[m];
         int ap = 0;
-        for(int apple_i=0; apple_i < m; apple_i++){
+        for (int apple_i = 0; apple_i < m; apple_i++) {
             apple[apple_i] = in.nextInt();
             if (a + apple[apple_i] >= s && a + apple[apple_i] <= t) {
                 ap++;
@@ -22,19 +20,15 @@ public class AppleAndOrange {
         }
         int[] orange = new int[n];
         int or = 0;
-        for(int orange_i=0; orange_i < n; orange_i++){
+        for (int orange_i = 0; orange_i < n; orange_i++) {
             orange[orange_i] = in.nextInt();
             if (b + orange[orange_i] >= s && b + orange[orange_i] <= t) {
                 or++;
             }
         }
-        
+
         System.out.println(ap);
         System.out.println(or);
-        
+
     }
 }
-
-
-
-
