778551a70c9768b08df6b558f5b8ac7349f814fa
Audarilanka <dissanayakelanka96@gmail.com>
2022-02-19 21:01:01 +0530

    feat(challenge 13):NumberLineJumps

--- Hackerrank/NumberLineJumps.java
+++ Hackerrank/NumberLineJumps.java
@@ -1,3 +1,21 @@
+import java.util.*;
+
 public class NumberLineJumps {
-    
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x1 = in.nextInt();
+        int v1 = in.nextInt();
+        int x2 = in.nextInt();
+        int v2 = in.nextInt();
+        
+        int vdiff = v1 - v2;
+        if (vdiff <= 0) {
+            System.out.println("NO");
+            return;
+        }
+        int xdiff = x1 - x2;
+        System.out.println(xdiff % vdiff == 0 ? "YES" : "NO");
+        in.close();
+    }
 }
