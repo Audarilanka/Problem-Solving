05e2d5178b772f10a71dc32e9f3300d80ffc084b
Audarilanka <dissanayakelanka96@gmail.com>
2022-02-27 18:33:06 +0530

    feat(challenges):DrawingBooks

--- Hackerrank/DrawingBooks.java
+++ Hackerrank/DrawingBooks.java
@@ -10,6 +10,7 @@ public class DrawingBooks {
         int frontdist = p / 2;
         int backdist = n % 2 == 0 ? (n - p + 1) / 2 : (n - p) / 2;
         System.out.println(Math.min(frontdist, backdist));
+        in.close();
         
     }
 }
