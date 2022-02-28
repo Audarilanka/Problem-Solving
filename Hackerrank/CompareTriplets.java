eeed50bdeed700648767efe35b3bdf157b8b6f19
Audarilanka <dissanayakelanka96@gmail.com>
2022-02-28 11:07:40 +0530

    feat(challenges):CompareTriplets

--- Hackerrank/CompareTriplets.java
+++ Hackerrank/CompareTriplets.java
@@ -4,23 +4,22 @@ import java.util.*;
 public class CompareTriplets {
 
     public static void main(String[] args) {
-        
-   
+
         Scanner in = new Scanner(System.in);
         int[] alice = new int[3];
         int[] bob = new int[3];
-        int a=0, b=0;
-        for(int i=0;i<3;i++)
-            alice[i]=in.nextInt();  
-        for(int i=0;i<3;i++)
-            bob[i]=in.nextInt();
-        for(int i=0;i<3;i++)
-            if(alice[i]>bob[i])
+        int a = 0, b = 0;
+        for (int i = 0; i < 3; i++)
+            alice[i] = in.nextInt();
+        for (int i = 0; i < 3; i++)
+            bob[i] = in.nextInt();
+        for (int i = 0; i < 3; i++)
+            if (alice[i] > bob[i])
                 a++;
-            else if(alice[i]<bob[i])
+            else if (alice[i] < bob[i])
                 b++;
-        System.out.println(a+" "+b);
+        System.out.println(a + " " + b);
         in.close();
-       
+
     }
 }
