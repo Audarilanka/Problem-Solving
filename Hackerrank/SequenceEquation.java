76cedbcd5aa336040bdf932cdd12bdb54f5ea85f
Audarilanka <dissanayakelanka96@gmail.com>
2022-03-11 11:54:39 +0530

    feat(challenges):Sequence Equation

--- Hackerrank/SequenceEquation.java
+++ Hackerrank/SequenceEquation.java
@@ -4,7 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class SequenceEquation {
     public static void main(String args[] ) throws Exception {
         Scanner in=new Scanner(System.in);
         int n=in.nextInt();
