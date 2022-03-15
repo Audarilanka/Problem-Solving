2de16381722c5d303c3f29f65d08c0c30bbbb72b
Audarilanka <dissanayakelanka96@gmail.com>
2022-03-15 11:25:03 +0530

    feat(challenges)CircularArrayRotation

---
+++ Hackerrank/CircularArrayRotation.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class CircularArrayRotation {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt(), k = in.nextInt(), q = in.nextInt();
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++) a[i] = in.nextInt();
+        for(int i = 0; i < q; i++) System.out.println(a[(n+((in.nextInt()-k)%n)) % n]);
+    }
+}
