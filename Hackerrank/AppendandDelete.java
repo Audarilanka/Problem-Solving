5db51b84867e3d4ca57b30467d95cdeda3bdcaa4
Audarilanka <dissanayakelanka96@gmail.com>
2022-03-20 20:20:58 +0530

    feat(challenges)

--- Hackerrank/AppendandDelete.java
+++ Hackerrank/AppendandDelete.java
@@ -7,37 +7,35 @@ public class AppendandDelete {
         String t = in.next();
         int k = in.nextInt();
         in.close();
-        
-      int sl=s.length();int tl=t.length();
-        int ll=sl>tl?tl:sl;
+
+        int sl = s.length();
+        int tl = t.length();
+        int ll = sl > tl ? tl : sl;
         int m;
-        for(m=0;m<ll;m++)
-            {
-            if(s.charAt(m)!=t.charAt(m))break;
-               
+        for (m = 0; m < ll; m++) {
+            if (s.charAt(m) != t.charAt(m))
+                break;
+
         }
-        
-        int sleft=sl-m;
-        int tleft=tl-m;
-        
-        int flag=0;
-        
-        if(sleft+tleft>k)flag=1;
-       else
-           {
-           int sub=k-(sleft+tleft);
-           if((sub%2!=0) && !(sub>2*m))flag=1;
-       }
-        
-        if(flag==0)
+
+        int sleft = sl - m;
+        int tleft = tl - m;
+
+        int flag = 0;
+
+        if (sleft + tleft > k)
+            flag = 1;
+        else {
+            int sub = k - (sleft + tleft);
+            if ((sub % 2 != 0) && !(sub > 2 * m))
+                flag = 1;
+        }
+
+        if (flag == 0)
             System.out.println("Yes");
-        else 
+        else
             System.out.println("No");
 
-           
-           
-           
-           
-       }
- 
-     }
+    }
+
+}
