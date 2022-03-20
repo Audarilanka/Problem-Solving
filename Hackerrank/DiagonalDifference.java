7a238972e55ee5fb5f8c5d4764ecdf08ccc4ff45
Audarilanka <dissanayakelanka96@gmail.com>
2022-03-20 20:23:26 +0530

    feat(challenges)DiagonalDifference

--- Hackerrank/DiagonalDifference.java
+++ Hackerrank/DiagonalDifference.java
@@ -4,33 +4,31 @@ public class DiagonalDifference {
 
     public static void main(String[] args) {
         int n;
-        Scanner numbers=new Scanner(System.in);
-        n=numbers.nextInt();
-        int a[][]=new int[n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                a[i][j]=numbers.nextInt();
+        Scanner numbers = new Scanner(System.in);
+        n = numbers.nextInt();
+        int a[][] = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = numbers.nextInt();
             }
         }
-        int x=0,y=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(j==i)
-                    x=x+a[i][j];
+        int x = 0, y = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == i)
+                    x = x + a[i][j];
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i==n-j-1){
-                    y=y+a[i][j];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == n - j - 1) {
+                    y = y + a[i][j];
                 }
             }
         }
-        int dif=y-x;
-        dif=Math.abs(dif);
+        int dif = y - x;
+        dif = Math.abs(dif);
         System.out.println(dif);
         numbers.close();
     }
 }
-
-
