5c1cbea7a40bdddd05cfc50d7d20260a9a610138
Audarilanka <dissanayakelanka96@gmail.com>
2022-05-30 16:30:34 +0530

    feat(challenges)BreakingTheRecords

--- Hackerrank/BreakingTheRecords.java
+++ Hackerrank/BreakingTheRecords.java
@@ -5,6 +5,7 @@ public class BreakingTheRecords {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int[] a = new int[n];
+
         for (int i = 0; i < n; i++)
             a[i] = in.nextInt();
         int min = a[0];
