24276276ce4b3409afafef9df94eb7d1e7f618c0
Audarilanka <dissanayakelanka96@gmail.com>
2022-06-10 17:53:12 +0530

    feat(challenges)

--- Hackerrank/DiagonalDifference.java
+++ Hackerrank/DiagonalDifference.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class DiagonalDifference {
 
     public static void main(String[] args) {
+
         int n;
         Scanner numbers = new Scanner(System.in);
         n = numbers.nextInt();
